21dd2058bd0863889de1a40a3e90a732c4dc41d2
sheridan-runs <sheridan@nurture.kiwi>
2026-01-21 00:28:46 +0000

    vercel-analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react' // <--- 1. Import added here
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics /> {/* <--- 2. Component added here */}
   </StrictMode>,
 )
